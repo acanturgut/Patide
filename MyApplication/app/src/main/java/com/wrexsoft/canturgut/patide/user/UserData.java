1034bedc869fb02c9dbb88198153c23a7468f0a2
Can Turgut <aturgut@ku.edu.tr>
2017-05-24 01:30:13 +0300

    user_screen layout done

--- MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/user/UserData.java
+++ MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/user/UserData.java
@@ -7,30 +7,12 @@ package com.wrexsoft.canturgut.patide.user;
 class UserData {
     private static final UserData userInstance = new UserData();
 
-    private String username;
-    private String email;
     private String userUID;
 
     static UserData getInstance() {
         return userInstance;
     }
     private UserData() {
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
     }
 
     public String getUserUID() {
---
+++ MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/user/user_from_data.java
@@ -0,0 +1,8 @@
+package com.wrexsoft.canturgut.patide.user;
+
+/**
+ * Created by CanTurgut on 24/05/2017.
+ */
+
+public class user_from_data {
+}
