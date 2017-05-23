fce0a536129e61cf4477b5d649047983452f1a6d
Selcuk Albayrak <mehmetalbayrak@ku.edu.tr>
2017-05-24 01:54:57 +0300

    Minor fixes

--- MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/UserFragment.java
+++ MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/UserFragment.java
@@ -10,6 +10,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -21,6 +23,14 @@ import com.google.firebase.auth.FirebaseUser;
 public class UserFragment extends Fragment {
 
     View view;
+
+    TextView mUserName;
+    Button mEdit;
+    EditText mEmail;
+    EditText mPassword;
+    EditText mLeisure;
+    EditText mWork;
+    EditText mStudy;
     Button mSignout;
 
     private FirebaseAuth mAuth;
@@ -38,8 +48,24 @@ public class UserFragment extends Fragment {
         // Inflate the layout for this fragment
         view = inflater.inflate(R.layout.fragment_user, container, false);
 
-        mSignout = (Button) view.findViewById(R.id.signout_button);
+        mUserName = (TextView) view.findViewById(R.id.username);
+        mEmail = (EditText) view.findViewById(R.id.etEmail);
+        mPassword = (EditText) view.findViewById(R.id.etPassword);
+        mLeisure = (EditText) view.findViewById(R.id.etLeisure);
+        mWork = (EditText) view.findViewById(R.id.etWork);
+        mStudy = (EditText) view.findViewById(R.id.etStudy);
 
+
+        mEdit = (Button) view.findViewById(R.id.edit_button);
+        mEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                mAuth.signOut();
+
+            }
+        });
+
+        mSignout = (Button) view.findViewById(R.id.signout_button);
         mSignout.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -47,6 +73,8 @@ public class UserFragment extends Fragment {
 
             }
         });
+
+
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
             public static final String TAG = "Firebase Auth";
