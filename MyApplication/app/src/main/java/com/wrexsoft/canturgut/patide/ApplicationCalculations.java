5289145a88b2e1cd4046cd11a40a685d2369d6eb
Taylan <ccapraz@ku.edu.tr>
2017-05-24 23:55:41 +0300

    sqlite

--- MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/ApplicationCalculations.java
+++ MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/ApplicationCalculations.java
@@ -1,20 +1,28 @@
 package com.wrexsoft.canturgut.patide;
 
+import java.util.Date;
+
 /**
  * Created by canta on 5/24/2017.
  */
 
 public class ApplicationCalculations {
-    public static String sene = "";
-    public static String ay = "";
-    public static String day = "";
-    public static String saat = "";
-    public static String dakika = "";
-
-    public static void calculate(String mydate){
-        String[] splited = mydate.split("\\s+");
+ 
+    public static Date getTime(String date){
+        String[] splited = date.split("\\s+");
+        String s1 = splited[0];
+        String[] dateValues = s1.split("/");
+        String s2 = splited[1];
+        String[] timeValues = s2.split(":");
+        Date mydate = new Date();
+        mydate.setYear( Integer.parseInt(dateValues[2]));
+        mydate.setMonth(Integer.parseInt(dateValues[1]));
+        mydate.setDate(Integer.parseInt(dateValues[0]));
+        mydate.setHours(Integer.parseInt(timeValues[0]));
+        mydate.setMinutes(Integer.parseInt(timeValues[1]));
+        return mydate;
     }
 
-    
+
 
 }
