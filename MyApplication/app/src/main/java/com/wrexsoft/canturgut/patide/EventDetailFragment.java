c58d07113f2d371ee4a9d4b6532b1b4462a89eca
Selcuk Albayrak <mehmetalbayrak@ku.edu.tr>
2017-05-25 02:26:23 +0300

    Event Detail Fragment logic

--- MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/EventDetailFragment.java
+++ MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/EventDetailFragment.java
@@ -1,6 +1,7 @@
 package com.wrexsoft.canturgut.patide;
 
 
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -8,7 +9,12 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.TimePicker;
 
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
@@ -28,7 +34,13 @@ public class EventDetailFragment extends Fragment {
     DatabaseReference dref;
     SharedPreferences settings;
 
-    TextView eventName;
+    EditText mEventName;
+    EditText mEstimatedTime;
+    EditText mComments;
+    Button ChooseDateButton;
+    Button AddEventButton;
+    TextView tvChooseDate;
+    SeekBar mPriority;
 
     public EventDetailFragment() {
         // Required empty public constructor
@@ -41,7 +53,19 @@ public class EventDetailFragment extends Fragment {
         // Inflate the layout for this fragment
         view =  inflater.inflate(R.layout.fragment_event_detail, container, false);
 
-        eventName = (TextView) view.findViewById(R.id.eventName);
+        mEventName = (EditText) view.findViewById(R.id.mEventName);
+        mEstimatedTime = (EditText) view.findViewById(R.id.mEstimatedTime);
+        mComments = (EditText) view.findViewById(R.id.mComments);
+
+        ChooseDateButton = (Button) view.findViewById(R.id.chooseDateBtn);
+/*        ChooseDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });*/
+        mPriority = (SeekBar) view.findViewById(R.id.choosePrioritySB);
+        AddEventButton = (Button) view.findViewById(R.id.editEventButton);
 
         final Bundle bundle = this.getArguments();
 
@@ -57,7 +81,11 @@ public class EventDetailFragment extends Fragment {
 
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                eventName.setText(dataSnapshot.child("eventname").getValue().toString());
+                mEventName.setText(dataSnapshot.child("eventname").getValue().toString());
+                mEstimatedTime.setText(dataSnapshot.child("estimatedtime").getValue().toString());
+                mComments.setText(dataSnapshot.child("comments").getValue().toString());
+                ChooseDateButton.setText(dataSnapshot.child("date").getValue().toString());
+                mPriority.setProgress(Integer.parseInt(dataSnapshot.child("eventname").getValue().toString()));
             }
 
             @Override
@@ -68,4 +96,85 @@ public class EventDetailFragment extends Fragment {
         return view;
     }
 
+    private void showDatePicker() {
+        final Dialog datepicker = new Dialog(getContext());
+
+        datepicker.setTitle("Pick a Date");
+
+        datepicker.setContentView(R.layout.dialog_date);
+
+        final DatePicker dPicker = (DatePicker) datepicker.findViewById(R.id.datePicker);
+        dPicker.setMinDate(System.currentTimeMillis() - 1000);
+        Button okBtn = (Button) datepicker.findViewById(R.id.btnDate);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                String day = "";
+                if (dPicker.getDayOfMonth() < 10) {
+                    day = "0";
+                }
+                String month = "";
+                if (dPicker.getMonth() < 10) {
+                    month = "0";
+                }
+                int  monthInt = dPicker.getMonth() + 1;
+                ChooseDateButton.setText(day + dPicker.getDayOfMonth() + "/" + month + Integer.toString(monthInt)+ "/" + dPicker.getYear());
+                showTimePicker();
+                datepicker.dismiss();
+
+            }
+        });
+
+        datepicker.show();
+
+    }
+
+    private void showTimePicker() {
+
+        final Dialog timepicker = new Dialog(getContext());
+
+        timepicker.setTitle("Pick a Time");
+
+        timepicker.setContentView(R.layout.dialog_time);
+
+        final TimePicker tPicker = (TimePicker) timepicker.findViewById(R.id.timePicker);
+        Button backBtn = (Button) timepicker.findViewById(R.id.back2date);
+        Button okBtn = (Button) timepicker.findViewById(R.id.timeOK);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String date = "" + ChooseDateButton.getText();
+
+                String hr = "";
+                if (tPicker.getHour() < 10) {
+                    hr = "0";
+                }
+                String min = "";
+                if (tPicker.getMinute() < 10) {
+                    min = "0";
+                }
+                ChooseDateButton.setText(date + " " + hr + tPicker.getHour() + ":" + min + tPicker.getMinute());
+                timepicker.dismiss();
+
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                timepicker.dismiss();
+                showDatePicker();
+
+            }
+        });
+
+        timepicker.show();
+    }
 }
