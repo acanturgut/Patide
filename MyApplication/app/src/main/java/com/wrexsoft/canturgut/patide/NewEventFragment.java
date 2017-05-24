55b340985350c4f24d3150d9115aa6bc681ca2a4
Selcuk Albayrak <mehmetalbayrak@ku.edu.tr>
2017-05-24 23:16:57 +0300

    Date and Time Pİcker Dialogs added

--- MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/NewEventFragment.java
+++ MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/NewEventFragment.java
@@ -1,6 +1,7 @@
 package com.wrexsoft.canturgut.patide;
 
 
+import android.app.Dialog;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
@@ -12,9 +13,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.SeekBar;
 import android.widget.TextView;
+import android.widget.TimePicker;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -69,6 +72,12 @@ public class NewEventFragment extends Fragment {
         mComments = (EditText) view.findViewById(R.id.mComments);
 
         ChooseDateButton = (Button) view.findViewById(R.id.chooseDateBtn);
+        ChooseDateButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                showDatePicker();
+            }
+        });
         mPriority = (SeekBar) view.findViewById(R.id.choosePrioritySB);
         AddEventButton = (Button) view.findViewById(R.id.addEventButton);
 
@@ -80,6 +89,69 @@ public class NewEventFragment extends Fragment {
             }
         });
         return view;
+    }
+
+    private void showDatePicker() {
+        final Dialog datepicker = new Dialog(getContext());
+
+        datepicker.setTitle("Pick a Date");
+
+        datepicker.setContentView(R.layout.dialog_date);
+
+        final DatePicker dPicker = (DatePicker) datepicker.findViewById(R.id.datePicker);
+        Button okBtn = (Button) datepicker.findViewById(R.id.btnDate);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                ChooseDateButton.setText(dPicker.getDayOfMonth() + "/" + dPicker.getMonth() + 1 + "/" + dPicker.getYear());
+                showTimePicker();
+                datepicker.dismiss();
+
+            }
+        });
+
+        datepicker.show();
+
+    }
+
+    private void showTimePicker() {
+
+        final Dialog timepicker = new Dialog(getContext());
+
+        timepicker.setTitle("Pick a Time");
+
+        timepicker.setContentView(R.layout.dialog_time);
+
+        final TimePicker tPicker = (TimePicker) timepicker.findViewById(R.id.timePicker);
+        Button backBtn = (Button) timepicker.findViewById(R.id.back2date);
+        Button okBtn = (Button) timepicker.findViewById(R.id.timeOK);
+
+        okBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                String date = "" + ChooseDateButton.getText();
+                ChooseDateButton.setText(date + " " + tPicker.getHour() + ":" + tPicker.getMinute());
+                timepicker.dismiss();
+
+            }
+        });
+
+        backBtn.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+
+                timepicker.dismiss();
+                showDatePicker();
+
+            }
+        });
+
+        timepicker.show();
     }
 
     private void GoToHome() {
