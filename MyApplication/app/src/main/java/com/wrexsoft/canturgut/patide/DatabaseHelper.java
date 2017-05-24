9d94e20ed893ba8038ea1ee7dd3eff1cd9f3fae4
Can Turgut <aturgut@ku.edu.tr>
2017-05-25 00:12:05 +0300

    chagnges

--- MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/DatabaseHelper.java
+++ MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/DatabaseHelper.java
@@ -23,8 +23,6 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String COL_7 = "PRIORITY";
 
 
-
-
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
     }
