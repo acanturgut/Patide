ae5f1d4bce22acde3d3d33180b56fd85eaa555c2
Taylan <ccapraz@ku.edu.tr>
2017-05-25 18:24:23 +0300

    more accurate calculation

--- MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/ApplicationCalculations.java
+++ MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/ApplicationCalculations.java
@@ -69,10 +69,10 @@ public class ApplicationCalculations {
             Log.d("calculations", "today: " + currentTime);
             Log.d("calculations", "event time: " + listOfEventDate[i]);
             long diff = listOfEventDate[i].getTime() - currentTime.getTime();
-            long diffDays = diff / (24 * 60 * 60 * 1000);
+            long diffDays = diff / (12 * 60 * 60 * 1000);
             Log.d("calculations", "Day difference" + diffDays);
-            Log.d("calculations", "Calculate: " + otherEvents + " + " + diffDays +"*" + (Integer.parseInt(leisure) + Integer.parseInt(work) + Integer.parseInt(study)));
-            otherEvents = otherEvents + (int)diffDays * (Integer.parseInt(leisure) + Integer.parseInt(work) + Integer.parseInt(study));
+            Log.d("calculations", "Calculate: " + otherEvents + " + " + diffDays +"*" + (Integer.parseInt(leisure) + Integer.parseInt(work) + Integer.parseInt(study)) + "/2");
+            otherEvents = otherEvents + (int)diffDays * ((Integer.parseInt(leisure) + Integer.parseInt(work) + Integer.parseInt(study)) / 2);
             Log.d("calculations", "Result is " + otherEvents);
             diff = listOfEventDate[i].getTime() - currentTime.getTime();
             diff = diff - (otherEvents*60*60*1000);
