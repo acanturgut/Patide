211845393b946fc0bc216b7edcb42357ee86ed46
Can Turgut <aturgut@ku.edu.tr>
2017-05-29 05:47:21 +0300

    notification time

---
+++ MyApplication/app/src/main/java/com/wrexsoft/canturgut/patide/AskScreenForCallender.java
@@ -0,0 +1,20 @@
+package com.wrexsoft.canturgut.patide;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.wrexsoft.canturgut.patide.R;
+
+public class AskScreenForCallender extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_ask_screen_for_callender);
+
+        getSupportActionBar().hide();
+
+
+
+    }
+}
